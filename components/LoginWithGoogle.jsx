7069d584010787e412a88210b91d12c16e04ffca
theonlyone <102159023+theonylon@users.noreply.github.com>
2023-09-04 15:01:59 -0300

    cambio_google

--- components/LoginWithGoogle.jsx
+++ components/LoginWithGoogle.jsx
@@ -1,27 +1,33 @@
-import { GoogleLogin } from 'react-google-login';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB_7E8_Ynnjhqf1dcGG5TJ7yx-mqyvPgH0",
+  authDomain: "critic-2f517.firebaseapp.com",
+  projectId: "critic-2f517",
+  storageBucket: "critic-2f517.appspot.com",
+  messagingSenderId: "360470519687",
+  appId: "1:360470519687:web:20bffc7f5f726c2c163d22",
+  measurementId: "G-PBNB27WGFK"
+};
 
+firebase.initializeApp(firebaseConfig);
 
+const provider = new firebase.auth.GoogleAuthProvider();
 
-const LoginWithGoogle = () => {
-  const responseGoogle = (response) => {
-    console.log(response);
-    // Aquí puedes manejar la respuesta del inicio de sesión de Google
+const Login = () => {
+  const handleLogin = async () => {
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <GoogleLogin
-      clientId="33243575474-hq1g51ta15hodu9sm1iolo7irrfp9el8.apps.googleusercontent.com"
+    <button onClick={handleLogin}>Iniciar sesión con Google</button>
+  );
+}
 
-      buttonText="Iniciar sesión con Google"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={'single_host_origin'}
-    />
-    
-      );
-};
-
-
-
-export default LoginWithGoogle;
+export default Login;
