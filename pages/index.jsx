890c17f9d787fa01cc883430e3aca790badd57d8
FaustoCitera <faustocitera@gmail.com>
2023-12-11 19:53:59 -0300

    back to life

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import '../public/globals.css';
+import Principal from './Principal';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Principal />
+    </div>
+  );
+};
+
+export default HomePage;
