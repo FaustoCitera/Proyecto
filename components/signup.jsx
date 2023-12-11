845188a8d56b460ab9b7a8a7e3fd9f401ec6f6cf
GaloResnik <101577483+GaloResnik@users.noreply.github.com>
2023-12-11 15:26:48 -0300

    signup logrado

--- components/signup.jsx
+++ components/signup.jsx
@@ -24,55 +24,65 @@ const SignUpPage = () => {
 
 
   const handleSubmit = async (e) => {
+
+    alert("corriendo")
     e.preventDefault();
     const username = e.target.querySelector("#username").value;
     const email = e.target.querySelector("#email").value;
     const password = e.target.querySelector("#password").value;
-    fetch('http://localhost:3001/signup', {
-      method: 'POST',
-      body: {
+
+    const body = JSON.stringify({
       username,
       email,
       password
-      }
+    });
+
+    console.log(body);
+
+    fetch('http://localhost:3001/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: body,
     }).then((r) => r.text())
       .then((d) => console.log(d))
       .catch((e) => console.log(e));
-    router.push('home');
+    //router.push('home');
 
   };
-    return (
-      <div className="page">
-        <div className="Iniciosign">
+  return (
+    <div className="page">
+      <div className="Iniciosign">
         <h2 className="h22">Crear cuenta</h2>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <label className="labelcolor">
-            <input className="inputcolor" type="email" id="email"placeholder="Email" value={email} onChange={handleEmailChange} />
-          </label>
-          <br />
-          <label className="labelcolor">
-            <input className="inputcolor" type="text" id="username" placeholder="Usuario" value={user} onChange={handleUserChange} />
-          </label>
-          <br />
-          <label className="labelcolor">
-            <input className="inputcolor" type="password" id="password" placeholder="Contraseña" value={password} onChange={handlePasswordChange} />
-          </label>
-          <br />
-          <label className="googlecolor">
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label className="labelcolor">
+          <input className="inputcolor" type="email" id="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label className="labelcolor">
+          <input className="inputcolor" type="text" id="username" placeholder="Usuario" value={user} onChange={handleUserChange} />
+        </label>
+        <br />
+        <label className="labelcolor">
+          <input className="inputcolor" type="password" id="password" placeholder="Contraseña" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <label className="googlecolor">
           <LoginWithGoogle />
-          </label>
-          <br />
-          <div className="columna">
+        </label>
+        <br />
+        <div className="columna">
           <button className="iniciobutton" type="submit">Crear cuenta</button>
-          <Link className= "volverbutton" href="/Principal">
+          <Link className="volverbutton" href="/Principal">
             Volver a la home
           </Link>
-          </div>
-        </form>
-      </div>
-      
-    );
-  };
+        </div>
+      </form>
+    </div>
+
+  );
+};
 
 export default SignUpPage;
