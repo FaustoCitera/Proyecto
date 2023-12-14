2e345c04329860d8883870225127a80e768e00e8
theonlyone <102159023+theonylon@users.noreply.github.com>
2023-12-14 12:08:15 -0300

    updategoogle

--- components/LoginWithGoogle.jsx
+++ components/LoginWithGoogle.jsx
@@ -21,14 +21,21 @@ const auth = getAuth();
 
 const Login = () => {
   const router = useRouter();
+
   const handleLogin = async () => {
     try {
-      await signInWithPopup(auth, provider);
-      router.push('home')
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Guarda información del usuario en el localStorage
+      localStorage.setItem('currentUser', JSON.stringify({
+        name: user.displayName,
+        email: user.email,
+        profilePic: user.photoURL,
+      }));
+
+      router.push('home');
     } catch (error) {
-
-      //es aca el redireccionar
-
       console.error(error);
     }
   };
@@ -37,40 +44,5 @@ const Login = () => {
     <button onClick={handleLogin}>‎ Iniciar sesión con Google</button>
   );
 }
-// // Función para iniciar sesión con correo electrónico y contraseña
-// export const signInWithEmail = (email, password) => {
-//   signInWithEmailAndPassword(auth, email, password)
-//     .then((userCredential) => {
-//       // Inicio de sesión exitoso
-//       const user = userCredential.user;
-//       const name = user.displayName;
-//       const profilePic = user.photoURL;
-      
-//       localStorage.setItem("name", name);
-//       localStorage.setItem("email", email);
-//       localStorage.setItem("profilePic", profilePic);
 
-//       alert("Log in exitoso");
-//        {
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       alert("La contraseña o el correo electrónico son incorrectos");
-//     });
-// };
-// // Función para registrar un nuevo usuario
-// export const registerUser = (email, password) => {
-//   auth.createUserWithEmailAndPassword(email, password)
-//     .then((userCredential) => {
-//       // Usuario registrado exitosamente
-//       const user = userCredential.user;
-//       console.log('Usuario registrado:', user);
-//       alert('Usuario registrado exitosamente');
-//     })
-//     .catch((error) => {
-//       console.error('Error al registrar usuario:', error.message);
-//       alert('Error al registrar usuario: ' + error.message);
-//     });
-// };
 export default Login;
